7e8752319aae55b1c768bcd2f402e124024605b2
mce123 <pmcelhiney@mce123.com>
2017-07-07 02:44:05 -0400

    Changes to setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
 
-sudo yum update
+sudo yum -y update
 sudo yum install -y python34
-sudo pip install --upgrade pip
-sudo yum install -y python3-pip
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip-py
-pip3 install jupyter
+sudo python3 get-pip.py
